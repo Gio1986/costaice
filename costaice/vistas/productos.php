248416b0abed36d75b5019146b613f6b6dd05421
Gio1986 <97552360+Gio1986@users.noreply.github.com>
2022-05-25 21:36:49 -0400

    Add files via upload

--- costaice/vistas/productos.php
+++ costaice/vistas/productos.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="../css/index.css" rel="stylesheet">
+    <link href="../css/productos.css" rel="stylesheet">
     <link rel="icon" href="../imagenes/logo2.jpg">
-    <title>CostaIce - Index</title>
+    <title>Document</title>
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -42,52 +42,5 @@
 </nav>
 <!--Fin Menu--> 
     
-<!--Carrusel-->
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../imagenes/carrusel1.jpg" class="d-block w-100" alt="680">
-    </div>
-    <div class="carousel-item">
-      <img src="../imagenes/carrusel2.jpg" class="d-block w-100" alt="680">
-    </div>
-    <div class="carousel-item">
-      <img src="../imagenes/img3.jpg" class="d-block w-100" alt="680">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<!-- Fin Carrusel--> 
-
-<!--Mapa-->
-<div id="map" class="map">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13322.874994719037!2d-70.72872994999999!3d-33.4045011!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2scl!4v1653525966839!5m2!1ses!2scl" 
-  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
-<!-- Fin Mapa--> 
-
-<!--Cuadro texto-->
-
-
-<!--Fin Cuadro texto-->
-
-<!-- Footer -->
-<!-- Copyright -->
-<div class="copyright">
-    © 2022 Derechos de Autor : 
-    <a class="text-reset-fw-bold">Pied Piper 
-      <img src="../imagenes/logo.png">
-    </a>
-  </div>
-  <!-- Copyright -->
-<!-- Footer -->
-
 </body>
 </html>
